3551d257d23a128a98e771968d7bb80ea20591e7
sunerlee <129197051+sunerlee@users.noreply.github.com>
2024-07-20 02:17:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lanceson.top": dockerHub,
+  "quay.lanceson.top": "https://quay.io",
+  "gcr.lanceson.top": "https://gcr.io",
+  "k8s-gcr.lanceson.top": "https://k8s.gcr.io",
+  "k8s.lanceson.top": "https://registry.k8s.io",
+  "ghcr.lanceson.top": "https://ghcr.io",
+  "cloudsmith.lanceson.top": "https://docker.cloudsmith.io",
+  "ecr.lanceson.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lanceson.top": dockerHub,
 };
 
 function routeByHosts(host) {
